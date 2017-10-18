8e9da80ae6a83d2576ae86df7a3563a7232cda96
Jennings Wu <jenningswu@gmail.com>
2017-10-18 01:15:04 -0700

    Clean up code

--- index.ios.js
+++ index.ios.js
@@ -7,9 +7,6 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View
 } from 'react-native';
 import KeyboardManager from 'react-native-keyboard-manager'
 
@@ -20,43 +17,5 @@ KeyboardManager.setEnableAutoToolbar(true);
 KeyboardManager.setToolbarPreviousNextButtonEnable(true)
 
 import App from './app'
-
-export default class FlatListOfTextInput extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('FlatListOfTextInput', () => App);
